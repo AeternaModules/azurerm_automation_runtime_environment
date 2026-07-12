e37d7072e88c49d33e418095e1134773bba4282f
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:04 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "automation_runtime_environments" {
-  description = "All automation_runtime_environment resources"
-  value       = azurerm_automation_runtime_environment.automation_runtime_environments
-}
 output "automation_runtime_environments_automation_account_id" {
-  description = "List of automation_account_id values across all automation_runtime_environments"
-  value       = [for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : v.automation_account_id]
+  description = "Map of automation_account_id values across all automation_runtime_environments, keyed the same as var.automation_runtime_environments"
+  value       = { for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : k => v.automation_account_id }
 }
 output "automation_runtime_environments_description" {
-  description = "List of description values across all automation_runtime_environments"
-  value       = [for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : v.description]
+  description = "Map of description values across all automation_runtime_environments, keyed the same as var.automation_runtime_environments"
+  value       = { for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : k => v.description }
 }
 output "automation_runtime_environments_location" {
-  description = "List of location values across all automation_runtime_environments"
-  value       = [for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : v.location]
+  description = "Map of location values across all automation_runtime_environments, keyed the same as var.automation_runtime_environments"
+  value       = { for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : k => v.location }
 }
 output "automation_runtime_environments_name" {
-  description = "List of name values across all automation_runtime_environments"
-  value       = [for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : v.name]
+  description = "Map of name values across all automation_runtime_environments, keyed the same as var.automation_runtime_environments"
+  value       = { for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : k => v.name }
 }
 output "automation_runtime_environments_runtime_default_packages" {
-  description = "List of runtime_default_packages values across all automation_runtime_environments"
-  value       = [for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : v.runtime_default_packages]
+  description = "Map of runtime_default_packages values across all automation_runtime_environments, keyed the same as var.automation_runtime_environments"
+  value       = { for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : k => v.runtime_default_packages }
 }
 output "automation_runtime_environments_runtime_language" {
-  description = "List of runtime_language values across all automation_runtime_environments"
-  value       = [for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : v.runtime_language]
+  description = "Map of runtime_language values across all automation_runtime_environments, keyed the same as var.automation_runtime_environments"
+  value       = { for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : k => v.runtime_language }
 }
 output "automation_runtime_environments_runtime_version" {
-  description = "List of runtime_version values across all automation_runtime_environments"
-  value       = [for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : v.runtime_version]
+  description = "Map of runtime_version values across all automation_runtime_environments, keyed the same as var.automation_runtime_environments"
+  value       = { for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : k => v.runtime_version }
 }
 output "automation_runtime_environments_tags" {
-  description = "List of tags values across all automation_runtime_environments"
-  value       = [for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : v.tags]
+  description = "Map of tags values across all automation_runtime_environments, keyed the same as var.automation_runtime_environments"
+  value       = { for k, v in azurerm_automation_runtime_environment.automation_runtime_environments : k => v.tags }
 }
 
